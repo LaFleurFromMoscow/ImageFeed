495e385a4505dce0d58cd399809c69dffd376494
LaFleurFromMoscow <ivanovskiyroma@mail.ruivanovskiyroma@mail.ruivanovskiyroma@mail.ru>
2023-08-26 10:46:12 +0300

    добавлена картинка, лайк и дата. работа с констрейнтами

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Роман Ивановский on 19.08.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    
+}
